1abcb33b276dc795351d55867cd3984bb6bf0d6f
hfccr <hfccr@outlook.com>
2024-11-20 00:12:10 +0530

    Add contract info

---
+++ util/deployment.ts
@@ -0,0 +1,9 @@
+type Deployment = {
+  confidentialToken: `0x${string}`;
+};
+
+const deployment: Deployment = {
+  confidentialToken: "0xae84335fbDA646737Cd316DCB4FbFda5167AC723",
+};
+
+export default deployment;
