b825434175c53827d5ba8127aed8064a228550d8
hfccr <hfccr@outlook.com>
2024-11-20 09:37:21 +0530

    Add contract address

--- components/ConfidentailToken.tsx
+++ components/ConfidentailToken.tsx
@@ -1,7 +1,16 @@
 "use client";
 import confidentailToken from "@/util/confidentialToken";
 import deployment from "@/util/deployment";
-import { CardContent, Paper, Skeleton, Stack, Typography } from "@mui/material";
+import { rivest } from "@/util/rivest";
+import {
+  CardContent,
+  Paper,
+  Skeleton,
+  Stack,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import { Addreth } from "addreth";
 import { useReadContract } from "wagmi";
 
 export default function ConfidentailToken() {
@@ -15,24 +24,36 @@ export default function ConfidentailToken() {
     address: deployment.confidentialToken,
     functionName: "symbol",
   });
+  const theme = useTheme();
   return (
     <>
       <Stack direction="column">
         <Paper variant="outlined">
           <CardContent>
-            <Stack
-              direction="row"
-              justifyContent="space-between"
-              alignItems="center"
-            >
-              <Typography variant="h4">
-                {isNameFetched && <>{name}</>}
-                {!isNameFetched && <Skeleton width={150} variant="text" />}
-              </Typography>
-              <Typography variant="h6">
-                {isSymbolFetched && <>${symbol}</>}
-                {!isSymbolFetched && <Skeleton width={50} variant="text" />}
-              </Typography>
+            <Stack direction="column" spacing={2}>
+              <Stack
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
+              >
+                <Typography variant="h4">
+                  {isNameFetched && <>{name}</>}
+                  {!isNameFetched && <Skeleton width={150} variant="text" />}
+                </Typography>
+                <Typography variant="h6">
+                  {isSymbolFetched && <>${symbol}</>}
+                  {!isSymbolFetched && <Skeleton width={50} variant="text" />}
+                </Typography>
+              </Stack>
+              <Addreth
+                address={deployment.confidentialToken}
+                icon={false}
+                theme={theme.palette.mode === "dark" ? "dark" : "light"}
+                explorer={(address) => ({
+                  name: "Rivest Explorer",
+                  accountUrl: `${rivest.blockExplorers.default.url}/address/${address}`,
+                })}
+              />
             </Stack>
           </CardContent>
         </Paper>
