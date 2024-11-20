94e2e78986bdcdfd2a786dc33a5f45dd51a42915
hfccr <hfccr@outlook.com>
2024-11-20 21:21:05 +0530

    Add roboto

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,10 @@ import { WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./global.css";
 import config from "@/util/wagmiConfig";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const NAVIGATION: Navigation = [
   {
