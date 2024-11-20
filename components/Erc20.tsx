4e6627737046d2b0a3b18546867ce26756c5fb7e
hfccr <hfccr@outlook.com>
2024-11-20 23:00:45 +0530

    Add elements

--- components/Erc20.tsx
+++ components/Erc20.tsx
@@ -1,9 +1,56 @@
-import { Card, CardHeader } from "@mui/material";
+"use client";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import PaidIcon from "@mui/icons-material/Paid";
+import { Addreth } from "addreth";
+import deployment from "@/util/deployment";
+import { rivest } from "@/util/rivest";
+import MintButton from "./MintButton";
+import usdcToken from "@/util/usdcToken";
+import BurnButton from "./BurnButton";
 
 export default function Erc20() {
+  const theme = useTheme();
   return (
     <Card variant="outlined">
-      <CardHeader title="ERC20 Token" />
+      <CardHeader
+        title={<Typography variant="h5">USD Coin</Typography>}
+        subheader={
+          <Addreth
+            address={deployment.usdcToken}
+            icon={false}
+            theme={theme.palette.mode === "dark" ? "dark" : "light"}
+            explorer={(address) => ({
+              name: "Rivest Explorer",
+              accountUrl: `${rivest.blockExplorers.default.url}/address/${address}`,
+            })}
+          />
+        }
+        avatar={<PaidIcon fontSize="large" />}
+        disableTypography
+        action={
+          <Typography variant="h6" sx={{ mt: 0.5, ml: 4, mr: 1 }}>
+            $USDC
+          </Typography>
+        }
+      />
+      <CardContent>
+        <MintButton
+          address={deployment.usdcToken}
+          abi={usdcToken.abi}
+          mintFunction="mint"
+        />
+        <BurnButton
+          address={deployment.usdcToken}
+          abi={usdcToken.abi}
+          burnFunction="burn"
+        />
+      </CardContent>
     </Card>
   );
 }
--- components/Wrapper.tsx
+++ components/Wrapper.tsx
@@ -1,9 +1,42 @@
-import { Card, CardHeader } from "@mui/material";
+"use client";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import LockIcon from "@mui/icons-material/Lock";
+import { Addreth } from "addreth";
+import deployment from "@/util/deployment";
+import { rivest } from "@/util/rivest";
 
 export default function Wrapper() {
+  const theme = useTheme();
   return (
     <Card variant="outlined">
-      <CardHeader title="Confidential Wrapper" />
+      <CardHeader
+        title={<Typography variant="h5">Wrapped cUSDC</Typography>}
+        subheader={
+          <Addreth
+            address={deployment.wrapper}
+            icon={false}
+            theme={theme.palette.mode === "dark" ? "dark" : "light"}
+            explorer={(address) => ({
+              name: "Rivest Explorer",
+              accountUrl: `${rivest.blockExplorers.default.url}/address/${address}`,
+            })}
+          />
+        }
+        avatar={<LockIcon fontSize="large" />}
+        disableTypography
+        action={
+          <Typography variant="h6" sx={{ mt: 0.5, ml: 4, mr: 1 }}>
+            $wcUSDC
+          </Typography>
+        }
+      />
+      <CardContent></CardContent>
     </Card>
   );
 }
