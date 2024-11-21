16a0c1622d267d4b2aaa534b2fd458f015b6c611
hfccr <hfccr@outlook.com>
2024-11-21 09:15:36 +0530

    Add more breakpoints

--- components/Caas.tsx
+++ components/Caas.tsx
@@ -5,10 +5,10 @@ import Wrapper from "./Wrapper";
 export default function Caas() {
   return (
     <Grid2 spacing={4} container>
-      <Grid2 size={{ sm: 12, md: 6 }}>
+      <Grid2 size={{ xs: 12, sm: 12, md: 12, lg: 6, xl: 6 }}>
         <Erc20 />
       </Grid2>
-      <Grid2 size={{ sm: 12, md: 6 }}>
+      <Grid2 size={{ xs: 12, sm: 12, md: 12, lg: 6, xl: 6 }}>
         <Wrapper />
       </Grid2>
     </Grid2>
