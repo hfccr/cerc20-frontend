6b4f77e179dab5514ea54d5c69f5dc2d21656a4b
hfccr <hfccr@outlook.com>
2024-11-22 12:35:25 +0530

    Add page to handle about link in compact sidebar

---
+++ app/(dashboard)/about/page.tsx
@@ -0,0 +1,7 @@
+import { Typography } from "@mui/material";
+
+export default function AboutPage() {
+  return (
+    <Typography>Learn more about FHE and Confidential ERC20 tokens</Typography>
+  );
+}
